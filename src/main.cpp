294699e1bf9fc6dd065fb88a4bddf934d567351e
fynnwilliam <fynnwilliam@gmail.com>
2021-07-26 00:02:43 -0400

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@ int main(int argc, char* argv[])
     catch (std::invalid_argument& e)
     {
         std::cout << e.what()
-            << "\nplease call with a specific location, region or IP"
-            << "\neg.1: " << argv[0] << " Los Angeles"
-            << "\neg.2: " << argv[0] << " Salta"
-            << "\neg.3: " << argv[0] << " GMT+2"
-            << "\neg.4: " << argv[0] << " 8.8.8.8"
-            << std::endl;
+                  << "\nplease call with a specific location, region or IP"
+                  << "\neg.1: " << argv[0] << " Los Angeles"
+                  << "\neg.2: " << argv[0] << " Salta"
+                  << "\neg.3: " << argv[0] << " GMT+2"
+                  << "\neg.4: " << argv[0] << " 8.8.8.8"
+                  << std::endl;
     }
 
     catch (std::out_of_range& e)
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
     catch (curlpp::RuntimeError& e)
     {
         std::cout << e.what()
-            << "\nplease check your internet connection or check for typos"
-            << std::endl;
+                  << "\nplease check your internet connection or check for typos"
+                  << std::endl;
     }
 }
