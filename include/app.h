53c2ff17ed59525b91cf87ed49ba3e6ebb26e5b4
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 01:32:51 -0400

    apply clang-format

--- include/app.h
+++ include/app.h
@@ -1,67 +1,70 @@
 #pragma once
-#include <regex>
-#include <vector>
-#include <iostream>
 #include <exception>
+#include <iostream>
+#include <regex>
 #include <unordered_map>
+#include <vector>
 
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Options.hpp>
 #include <curlpp/Exception.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/cURLpp.hpp>
 
 #include "timezn.h"
 
-class app
-{
+class app {
 private:
-    int argc_{};
-    char** argv_;
-    std::string usr_input_;
-    std::string url_{"http://worldtimeapi.org/api"};
-    std::string abbreviation_;
-    std::string time_;
-    std::string date_;
-    std::string day_;
-    std::stringstream fetched_;
+  int argc_{};
+  char **argv_;
+  std::string usr_input_;
+  std::string url_{"http://worldtimeapi.org/api"};
+  std::string abbreviation_;
+  std::string time_;
+  std::string date_;
+  std::string day_;
+  std::stringstream fetched_;
 
-    std::unordered_map<std::string, timezn> timezns_;
-    std::vector<std::string> const days_of_week_{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    std::vector<std::string> const months_{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-    
-    bool private_a(std::string const&) const;
-    bool private_b(std::string const&) const;
-    bool private_c(std::string const&) const;
-    bool private_ip(std::string const&) const;
-    bool public_ip(std::string const&) const;
-    bool ip_address(std::string const&) const;
-    std::string& to_upper(std::string&);
-    std::string try_uppercase();
-    std::string datetime();
-    std::string capitalize(std::string);
-    std::string to_lower(std::string);
-    std::string ip() const;
-    std::string location();
-    std::string find_timezone();
-    std::string preposition() const;
-    std::string _datetime() const;
-    std::string usr_input() const;
-    void _transform(std::string&, int);
-    void time(std::string const&);
-    void abbreviation(std::string const&);
-    void day(std::string const&);
-    inline std::string _day(int d) const { return days_of_week_[d]; }
-    inline std::string _month(int m) const { return months_[m - 1]; }
-    std::string month(std::string const&) const;
-    void date(std::string const&);
-    void read_timezones();
-    void check_arguments();
-    void update_url();
-    void fetch_time();
-    void display_time();
-    auto invalid_argc() const;
-    auto not_avaliable() const;
+  std::unordered_map<std::string, timezn> timezns_;
+  std::vector<std::string> const days_of_week_{
+      "Sunday",   "Monday", "Tuesday", "Wednesday",
+      "Thursday", "Friday", "Saturday"};
+  std::vector<std::string> const months_{
+      "January", "February", "March",     "April",   "May",      "June",
+      "July",    "August",   "September", "October", "November", "December"};
+
+  bool private_a(std::string const &) const;
+  bool private_b(std::string const &) const;
+  bool private_c(std::string const &) const;
+  bool private_ip(std::string const &) const;
+  bool public_ip(std::string const &) const;
+  bool ip_address(std::string const &) const;
+  std::string &to_upper(std::string &);
+  std::string try_uppercase();
+  std::string datetime();
+  std::string capitalize(std::string);
+  std::string to_lower(std::string);
+  std::string ip() const;
+  std::string location();
+  std::string find_timezone();
+  std::string preposition() const;
+  std::string _datetime() const;
+  std::string usr_input() const;
+  void _transform(std::string &, int);
+  void time(std::string const &);
+  void abbreviation(std::string const &);
+  void day(std::string const &);
+  inline std::string _day(int d) const { return days_of_week_[d]; }
+  inline std::string _month(int m) const { return months_[m - 1]; }
+  std::string month(std::string const &) const;
+  void date(std::string const &);
+  void read_timezones();
+  void check_arguments();
+  void update_url();
+  void fetch_time();
+  void display_time();
+  auto invalid_argc() const;
+  auto not_avaliable() const;
 
 public:
-    app(int i, char** c) : argc_{i}, argv_{c} {}
-    void tell();
+  app(int i, char **c) : argc_{i}, argv_{c} {}
+  void tell();
 };
--- src/app.cpp
+++ src/app.cpp
@@ -1,217 +1,193 @@
 #include "app.h"
 #include "timezones.h"
 
-void app::tell()
-{
-    check_arguments();
-    read_timezones();
-    update_url();
-    fetch_time();
-    display_time();
+void app::tell() {
+  check_arguments();
+  read_timezones();
+  update_url();
+  fetch_time();
+  display_time();
 }
 
-void app::fetch_time()
-{
-    fetched_ << curlpp::options::Url(url_);
+void app::fetch_time() { fetched_ << curlpp::options::Url(url_); }
+
+auto app::invalid_argc() const {
+  return std::invalid_argument("invalid number of arguments");
 }
 
-auto app::invalid_argc() const
-{
-    return std::invalid_argument("invalid number of arguments");
+auto app::not_avaliable() const {
+  return std::range_error("timezone not found");
 }
 
-auto app::not_avaliable() const
-{
-    return std::range_error("timezone not found");
+void app::check_arguments() {
+  if (argc_ > 3 || argc_ == 1) {
+    throw invalid_argc();
+  }
+
+  usr_input_ = ip_address(argv_[1]) ? ip() : location();
 }
 
-void app::check_arguments()
-{
-    if (argc_ > 3 || argc_ == 1) { throw invalid_argc(); }
+void app::read_timezones() {
+  std::stringstream timezones{::timezones};
+  std::string timezone;
 
-    usr_input_ = ip_address(argv_[1]) ? ip() : location();
+  while (timezones >> timezone) {
+    timezn temp = timezn().values(timezone);
+    timezns_.insert({temp.name(), temp});
+  }
 }
 
-void app::read_timezones()
-{
-    std::stringstream timezones{::timezones};
-    std::string timezone;
+void app::update_url() {
+  std::string zone{find_timezone()};
+  zone.empty() ? throw not_avaliable() : url_.append(zone).append(".txt");
+}
 
-    while (timezones >> timezone)
-    {
-        timezn temp = timezn().values(timezone);
-        timezns_.insert({temp.name(), temp});
+void app::time(std::string const &t) {
+  time_ = std::string{"It is "} + t.substr(t.find('T') + 1, 8);
+}
+
+void app::abbreviation(std::string const &a) {
+  abbreviation_ = std::string{" "}.append(a);
+}
+
+void app::day(std::string const &d) {
+  day_ = std::string{", "} + _day(std::stoi(d));
+}
+
+std::string app::month(std::string const &m) const {
+  return std::string{", "} + _month(std::stoi(m));
+}
+
+void app::date(std::string const &d) {
+  std::string year{d.substr(0, 4)};
+  std::string m{d.substr(5, 2)};
+  int day{std::stoi(d.substr(8, 2))};
+
+  date_ = month(m) + ' ' + std::to_string(day) + ", " + year;
+}
+
+std::string app::preposition() const {
+  return ip_address(usr_input_) ? " at " : " in ";
+}
+
+std::string app::usr_input() const {
+  std::string s{usr_input_};
+  std::replace(begin(s), end(s), '_', ' ');
+  return s;
+}
+
+std::string app::_datetime() const {
+  return time_ + abbreviation_ + day_ + date_ + preposition() + usr_input();
+}
+
+std::string app::datetime() {
+  std::string temp;
+  int count{};
+
+  while (fetched_ >> temp) {
+    if (count == 1) {
+      abbreviation(temp);
     }
-}
-
-void app::update_url()
-{   
-    std::string zone{find_timezone()};
-    zone.empty() ? throw not_avaliable() : url_.append(zone).append(".txt");
-}
-
-void app::time(std::string const& t)
-{
-    time_ = std::string{"It is "} + t.substr(t.find('T') + 1, 8);
-}
-
-void app::abbreviation(std::string const& a)
-{
-    abbreviation_ = std::string{" "}.append(a);
-}
-
-void app::day(std::string const& d)
-{   
-    day_ = std::string{", "} + _day(std::stoi(d));
-}
-
-std::string app::month(std::string const& m) const
-{
-    return std::string{", "} + _month(std::stoi(m));
-}
-
-void app::date(std::string const& d)
-{
-    std::string year{d.substr(0, 4)};
-    std::string m{d.substr(5, 2)};
-    int day{std::stoi(d.substr(8, 2))};
-    
-    date_ = month(m) + ' ' + std::to_string(day) + ", " + year;
-}
-
-std::string app::preposition() const
-{
-    return ip_address(usr_input_) ? " at " : " in ";
-}
-
-std::string app::usr_input() const
-{
-    std::string s{usr_input_};
-    std::replace(begin(s), end(s), '_', ' ');
-    return s;
-}
-
-std::string app::_datetime() const
-{   
-    return time_ + abbreviation_ + day_ + date_ + preposition() + usr_input();
-}
-
-std::string app::datetime()
-{
-    std::string temp;
-    int count{};
-    
-    while (fetched_ >> temp)
-    {
-        if (count == 1) { abbreviation(temp); }
-        if (count == 5) { date(temp); time(temp); }
-        if (count == 7)
-        {
-            day(temp);
-            return _datetime();
-        }
-        count++;
+    if (count == 5) {
+      date(temp);
+      time(temp);
     }
-    
-    return std::string{"please update the app"};
+    if (count == 7) {
+      day(temp);
+      return _datetime();
+    }
+    count++;
+  }
+
+  return std::string{"please update the app"};
 }
 
-void app::display_time()
-{
-    std::cout << datetime() << std::endl;
+void app::display_time() { std::cout << datetime() << std::endl; }
+
+void app::_transform(std::string &s, int index) {
+  std::transform(s.begin(), s.begin() + index, s.begin(),
+                 [](unsigned char c) { return std::toupper(c); });
 }
 
-void app::_transform(std::string& s, int index)
-{
-    std::transform(s.begin(), s.begin() + index, s.begin(), [](unsigned char c)
-                   { return std::toupper(c); });
+std::string app::to_lower(std::string s) {
+  std::transform(s.begin(), s.end(), s.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
+
+  return s;
 }
 
-std::string app::to_lower(std::string s)
-{
-    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c)
-                   { return std::tolower(c); });
+std::string app::capitalize(std::string s) {
+  to_lower(s.substr(0, 3)) == "gmt" ? _transform(s, 3) : _transform(s, 1);
 
-    return s;
+  return s;
 }
 
-std::string app::capitalize(std::string s)
-{
-    to_lower(s.substr(0, 3)) == "gmt" ? _transform(s, 3) : _transform(s, 1);
+std::string app::ip() const {
+  if (argc_ > 2) {
+    throw invalid_argc();
+  }
 
-    return s;
+  std::string ip{argv_[1]};
+
+  if (public_ip(ip)) {
+    return ip;
+  }
+  throw std::out_of_range("\"" + ip + "\" is not a public IP");
 }
 
-std::string app::ip() const
-{
-    if (argc_ > 2) { throw invalid_argc(); }
+std::string app::location() {
+  std::string first = capitalize(argv_[1]);
+  std::string second = !argv_[2] ? std::string{} : capitalize(argv_[2]);
 
-    std::string ip{argv_[1]};
-
-    if (public_ip(ip)) { return ip; }
-    throw std::out_of_range("\"" + ip + "\" is not a public IP");
+  return second.empty() ? first : first.append('_' + second);
 }
 
-std::string app::location()
-{
-    std::string first = capitalize(argv_[1]);
-    std::string second = !argv_[2] ? std::string{} : capitalize(argv_[2]);
+std::string &app::to_upper(std::string &s) {
+  std::transform(s.begin(), s.end(), s.begin(),
+                 [](unsigned char c) { return std::toupper(c); });
 
-    return second.empty() ? first : first.append('_' + second);
+  return s;
 }
 
-std::string& app::to_upper(std::string& s)
-{
-    std::transform(s.begin(), s.end() , s.begin(), [](unsigned char c)
-                  { return std::toupper(c); });
-    
-    return s;
+std::string app::try_uppercase() {
+  auto T = timezns_.find(to_upper(usr_input_));
+  return T == timezns_.end() ? std::string{} : T->second.sub_link();
 }
 
-std::string app::try_uppercase()
-{
-    auto T = timezns_.find(to_upper(usr_input_));
-    return T == timezns_.end() ? std::string{} : T->second.sub_link();
+std::string app::find_timezone() {
+  if (ip_address(usr_input_)) {
+    return std::string{"/ip/"}.append(usr_input_);
+  }
+
+  auto t = timezns_.find(usr_input_);
+  return t == timezns_.end() ? try_uppercase() : t->second.sub_link();
 }
 
-std::string app::find_timezone()
-{
-    if (ip_address(usr_input_)) { return std::string{"/ip/"}.append(usr_input_); }
-
-    auto t = timezns_.find(usr_input_);
-    return t == timezns_.end() ? try_uppercase() : t->second.sub_link();
+bool app::private_a(std::string const &ip) const {
+  std::regex pattern("([1][0])(\\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])){3}");
+  return std::regex_match(ip, pattern);
 }
 
-bool app::private_a(std::string const& ip) const
-{
-    std::regex pattern("([1][0])(\\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])){3}");
-    return std::regex_match(ip, pattern);
+bool app::private_b(std::string const &ip) const {
+  std::regex pattern("([1][7][2])(\\.(1[6-9]|2[0-9]|3[01]))(\\.([01]?[0-9][0-9]"
+                     "?|2[0-4][0-9]|25[0-5])){2}");
+  return std::regex_match(ip, pattern);
 }
 
-bool app::private_b(std::string const& ip) const
-{
-    std::regex pattern("([1][7][2])(\\.(1[6-9]|2[0-9]|3[01]))(\\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])){2}");
-    return std::regex_match(ip, pattern);
+bool app::private_c(std::string const &ip) const {
+  std::regex pattern("([1][9][2])(\\.([1][6][8]))(\\.([01]?[0-9][0-9]?|2[0-4]["
+                     "0-9]|25[0-5])){2}");
+  return std::regex_match(ip, pattern);
 }
 
-bool app::private_c(std::string const& ip) const
-{
-    std::regex pattern("([1][9][2])(\\.([1][6][8]))(\\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])){2}");
-    return std::regex_match(ip, pattern);
+bool app::private_ip(std::string const &ip) const {
+  return private_a(ip) || private_b(ip) || private_c(ip);
 }
 
-bool app::private_ip(std::string const& ip) const
-{
-    return private_a(ip) || private_b(ip) || private_c(ip);
-}
+bool app::public_ip(std::string const &ip) const { return !private_ip(ip); }
 
-bool app::public_ip(std::string const& ip) const
-{
-    return !private_ip(ip);
-}
-
-bool app::ip_address(std::string const& item) const
-{
-    std::regex pattern("([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])(\\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])){3}");
-    return std::regex_match(item, pattern);
+bool app::ip_address(std::string const &item) const {
+  std::regex pattern("([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])(\\.([01]?[0-9][0-"
+                     "9]?|2[0-4][0-9]|25[0-5])){3}");
+  return std::regex_match(item, pattern);
 }
