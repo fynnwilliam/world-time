0c3ab167ed2fb1e18a8943a80bf0c49804a4da9d
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 13:38:04 -0400

    apply clang-format

--- include/timezn.h
+++ include/timezn.h
@@ -1,23 +1,22 @@
 #pragma once
-#include <string>
-#include <sstream>
 #include <algorithm>
+#include <sstream>
+#include <string>
 
-class timezn
-{
+class timezn {
 private:
-    std::string name_;
-    std::string region_;
-    std::string location_;
-    std::string area_;
-    
-    std::string try_insert_slash(std::string const&) const;
+  std::string name_;
+  std::string region_;
+  std::string location_;
+  std::string area_;
+
+  std::string try_insert_slash(std::string const &) const;
 
 public:
-    timezn values(std::string&);
-    std::string sub_link() const;
-    auto const& name() const noexcept { return name_; }
-    auto const& region() const noexcept { return region_; }
-    auto const& location() const noexcept { return location_; }
-    auto const& area() const noexcept { return area_; }
+  timezn values(std::string &);
+  std::string sub_link() const;
+  auto const &name() const noexcept { return name_; }
+  auto const &region() const noexcept { return region_; }
+  auto const &location() const noexcept { return location_; }
+  auto const &area() const noexcept { return area_; }
 };
