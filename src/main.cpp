16ce31e967a03846a67a2ebf5e557079e90ba02d
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 00:31:40 -0400

    apply clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,30 @@
 #include "app.h"
 
-int main(int argc, char* argv[])
-{
-    try
-    {
-        app time(argc, argv);
-        time.tell();
-    }
+int main(int argc, char *argv[]) {
+  try {
+    app time(argc, argv);
+    time.tell();
+  }
 
-    catch (std::invalid_argument& e)
-    {
-        std::cout << e.what()
-                  << "\nplease call with a specific location, region or IP"
-                  << "\neg.1: " << argv[0] << " Los Angeles"
-                  << "\neg.2: " << argv[0] << " Salta"
-                  << "\neg.3: " << argv[0] << " GMT+2"
-                  << "\neg.4: " << argv[0] << " 8.8.8.8"
-                  << std::endl;
-    }
+  catch (std::invalid_argument &e) {
+    std::cout << e.what()
+              << "\nplease call with a specific location, region or IP"
+              << "\neg.1: " << argv[0] << " Los Angeles"
+              << "\neg.2: " << argv[0] << " Salta"
+              << "\neg.3: " << argv[0] << " GMT+2"
+              << "\neg.4: " << argv[0] << " 8.8.8.8" << std::endl;
+  }
 
-    catch (std::out_of_range& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
+  catch (std::out_of_range &e) {
+    std::cout << e.what() << std::endl;
+  }
 
-    catch (std::range_error& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    
-    catch (curlpp::RuntimeError& e)
-    {
-        std::cout << e.what()
-                  << "\nplease check your internet connection"
-                  << std::endl;
-    }
+  catch (std::range_error &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  catch (curlpp::RuntimeError &e) {
+    std::cout << e.what() << "\nplease check your internet connection"
+              << std::endl;
+  }
 }
