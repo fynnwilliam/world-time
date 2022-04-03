c51dcd17ecbf5d1bd2859a41b79368fdd441311e
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 00:03:55 -0400

    apply clang-format

--- src/timezn.cpp
+++ src/timezn.cpp
@@ -1,25 +1,20 @@
 #include "timezn.h"
 
-timezn timezn::values(std::string& timezone)
-{
-    std::replace(timezone.begin(), timezone.end(), '/', ' ');
-    std::stringstream temp(timezone);
-    temp >> region_ >> location_ >> area_;
+timezn timezn::values(std::string &timezone) {
+  std::replace(timezone.begin(), timezone.end(), '/', ' ');
+  std::stringstream temp(timezone);
+  temp >> region_ >> location_ >> area_;
 
-    name_ = area_ == location_ ? region_ : area_.empty() ? location_ : area_;
+  name_ = area_ == location_ ? region_ : area_.empty() ? location_ : area_;
 
-    return *this;
+  return *this;
 }
 
-std::string timezn::sub_link() const
-{
-    return std::string{"/timezone"}
-        + insert_slash(region())
-        + insert_slash(location())
-        + insert_slash(area());
+std::string timezn::sub_link() const {
+  return std::string{"/timezone"} + insert_slash(region()) +
+         insert_slash(location()) + insert_slash(area());
 }
 
-std::string timezn::insert_slash(std::string const& item) const
-{
-    return item.empty() ? item : '/' + item;
+std::string timezn::insert_slash(std::string const &item) const {
+  return item.empty() ? item : '/' + item;
 }
