3597d7288532d52caee1648b03a436830d5a2f87
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 00:23:49 -0400

    Inline is applied implicitly.

--- include/timezn.h
+++ include/timezn.h
@@ -16,8 +16,8 @@ private:
 public:
     timezn values(std::string&);
     std::string sub_link() const;
-    inline auto const& name() const noexcept { return name_; }
-    inline auto const& region() const noexcept { return region_; }
-    inline auto const& location() const noexcept { return location_; }
-    inline auto const& area() const noexcept { return area_; }
+    auto const& name() const noexcept { return name_; }
+    auto const& region() const noexcept { return region_; }
+    auto const& location() const noexcept { return location_; }
+    auto const& area() const noexcept { return area_; }
 };
