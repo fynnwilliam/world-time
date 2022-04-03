65a4a12ac64875febf5dfb515b77328f06857012
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 14:03:09 -0400

    Declare timezns class

---
+++ include/timezns.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <timezn.h>
+#include <unordered_map>
+
+class timezns {
+private:
+  std::unordered_map<std::string, timezn> timezns_;
+
+public:
+  timezns() {}
+
+  find() const noexcept;
+}
