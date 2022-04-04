303153e82abcb306602c01f5b67d195a09bdaca5
fynnwilliam <fynnwilliam@gmail.com>
2022-04-03 22:41:19 -0400

    Create a day class...

---
+++ include/day.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class day {
+private:
+  std::vector<std::string> const days_{"Sunday",    "Monday",   "Tuesday",
+                                       "Wednesday", "Thursday", "Friday",
+                                       "Saturday"};
+
+public:
+  std::string const &operator[](std::size_t day) const noexcept {
+    return day < days_.size() ? std::string{", "} + days_[day]
+                              : std::string{", Unknown_day"};
+  }
+};
