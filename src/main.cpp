2883d343294d16881758925f594d3a92f84cccd2
fynnwilliam <fynnwilliam@gmail.com>
2022-04-04 17:39:52 -0400

    Remove try - catch blocks from main
    We are virtually throwing nothing to be handled...

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,6 @@
 #include "app.h"
 
 int main(int argc, char *argv[]) {
-  try {
     app time{};
     time.tell(argc, argv);
-  }
-
-  catch (std::invalid_argument &e) {
-    std::cout << e.what()
-              << "\nplease call with a specific location, region or IP"
-              << "\neg.1: " << argv[0] << " Los Angeles"
-              << "\neg.2: " << argv[0] << " Salta"
-              << "\neg.3: " << argv[0] << " GMT+2"
-              << "\neg.4: " << argv[0] << " 8.8.8.8" << std::endl;
-  }
-
-  catch (std::out_of_range &e) {
-    std::cout << e.what() << std::endl;
-  }
 }
