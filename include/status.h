6119b2d28e1c37627817bb797d3bbb9d5ec3198f
fynnwilliam <fynnwilliam@gmail.com>
2022-04-04 21:16:03 -0400

    Update class status to use std::string

--- include/status.h
+++ include/status.h
@@ -1,20 +1,22 @@
 #pragma once
 
-#include <string_view>
+#include <string>
 
 class status {
 private:
-  int code_ {};
-  std::string_view message_;
+  int code_{};
+  std::string message_;
 
 public:
   status() = default;
   status(int i) : code_{i} {}
-  status(std::string_view m) : message_{std::move(m)} {}
-  status(int i, std::string_view m) : code_{i}, message_{std::move(m)} {}
+  status(std::string m) : message_{std::move(m)} {}
+  status(int i, std::string m) : code_{i}, message_{std::move(m)} {}
+
+  auto message() const noexcept { std::cout << message_; }
 
   operator bool() const noexcept {
-    std::cout << message_;
+    message();
     return code_ == 0;
   }
 };
